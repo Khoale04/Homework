68da2e332ea8da98c6217943021160172c577945
Khoale04 <khoa.le0498@gmail.com>
2023-11-21 12:43:47 +1100

    Update question 2

---
+++ Week 2/compute_of_circle.py
@@ -0,0 +1,7 @@
+str_radius = input("please enter the radius: ")
+radius = int(str_radius)
+pi = 3.1415
+int_compute = pi * (radius ** 2)
+compute = str(int_compute)
+str_result = "The result is " + compute
+print(str_result)
--- Week 2/hello_user.py
+++ Week 2/hello_user.py
@@ -1,4 +1,4 @@
 str_first_name = input('please insert your first name: ')
 str_last_name = input('please insert your last name: ')
 str_greeting_message = "Hello " + str_first_name + " " + str_last_name + " to our website"
-print (str_greeting_message)
+print(str_greeting_message)
