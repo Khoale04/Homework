1fee78a2f69eb26a31f6b858069f02f811804513
Khoale04 <khoa.le0498@gmail.com>
2023-11-21 12:48:14 +1100

    update question 3

---
+++ Week 2/rectangle_compute.py
@@ -0,0 +1,8 @@
+str_length = input("please enter the length: ")
+length = int(str_length)
+str_width = input("please enter the width: ")
+width = int(str_width)
+int_compute = length * width
+compute = str(int_compute)
+str_result = "The result is " + compute
+print(str_result)
