0451637e50beccafd0e8fe2c9e065ed23aa16bd2
Khoale04 <khoa.le0498@gmail.com>
2023-11-24 14:59:17 +1100

    update question 5

---
+++ Week 2/inch_to_feet.py
@@ -0,0 +1,5 @@
+str_inch = input('please enter inch: ')
+inch = int(str_inch)
+feet = inch * 0.083
+str_result = str(feet) + " feet"
+print(str_result)
---
+++ Week 2/simple_calculator.py
@@ -0,0 +1,8 @@
+str_num1 = input("please enter num1: ")
+num1 = int(str_num1)
+str_num2 = input("please enter num2: ")
+num2 = int(str_num2)
+str_operator = input("please enter operator: ")
+operator = int(str_operator)
+result = num1 + str_operator + num2
+print(result)
