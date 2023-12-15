90b93a83d857d60927f54939bb0f2abd5160bce6
Khoale04 <khoa.le0498@gmail.com>
2023-12-15 11:34:47 +1100

    update

--- week 4/list/list_q8.py
+++ week 4/list/list_q8.py
@@ -23,6 +23,7 @@
 
 
 list1 = [5, 20, 15, 20, 25, 50, 20]
-for i in range(0, len(list1), 1):
-    list1.remove(20)
-    print(list1)
+for i in list1:
+    if i == 20:
+        list1.remove(20)
+        print(list1)
