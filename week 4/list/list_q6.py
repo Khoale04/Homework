8e05b98670c0eb1cd7ea1b9a268424dda48d1a5d
Khoale04 <khoa.le0498@gmail.com>
2023-12-15 12:22:46 +1100

    update

--- week 4/list/list_q6.py
+++ week 4/list/list_q6.py
@@ -1,11 +1,18 @@
-# Add new item to list after a specified item. Write a program to add item 7000 after 6000 in the following Python List
+# Add new item to list after a specified item. Write a program to add item
+# 7000 after 6000 in the following Python List
 # Given:
 
 list1 = [10, 20, [300, 400, [5000, 6000], 500], 30, 40]
-list2 = list1[2]
-list3 = list2[2]
 
-if "6000" in list1 or list2:
-    print(7000)
+for i in list1:
+    if i == 6000:
+        list1.insert(7000)
+    else:
+
+
+
+
+
+
 
 
--- week 4/list/list_q7.py
+++ week 4/list/list_q7.py
@@ -1,4 +1,10 @@
 list1 = [5, 10, 15, 20, 25, 50, 20]
-# if "20" in list1:
-list1.sort()
+
+for i in range(0,len(list1),1):
+    print(list1[i])
+    if list1[i] == 20:
+        list1[i] = 200
+        break
+
 print(list1)
+
